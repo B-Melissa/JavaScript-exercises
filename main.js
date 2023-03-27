64b4f737b640d6c17e65df74a3951831334a1186
B-Melissa <bormethm@gmail.com>
2023-03-27 15:01:16 +0200

    Exercises_functions and loops

--- main.js
+++ main.js
@@ -63,14 +63,118 @@ console.log(result);
 
 //Exercise 6 - Sorting an Array and "for loop"
 
-const PurplePhoenix = ["Marta", "Alireza", "Sonja", "Sabir", "Mila", "Phouc", "Raul", "Melissa",];
-PurplePhoenix.sort();
+// const PurplePhoenix = ["Marta", "Alireza", "Sonja", "Sabir", "Mila", "Phouc", "Raul", "Melissa"];
+// PurplePhoenix.sort();
 
- console.log(PurplePhoenix[0]);
- console.log(PurplePhoenix[7]);
+//  console.log(PurplePhoenix[0]);
+//  console.log(PurplePhoenix[7]);
 
-for (let i = 0; i < 1; i++) {
-   console.log(PurplePhoenix);
+// for (let i = 0; i < 1; i++) {
+//    console.log(PurplePhoenix);
+// }
+
+//Exercise 7 Array filter() Methode
+
+// let PurplePhoenixAge = [10, 13, 16, 15, 14, 9, 28, 22];
+// PurplePhoenixAge.sort(function (a, b) { return a - b });
+// const odds = PurplePhoenixAge.filter((num) => num % 2 === 1);
+// console.log(odds);
+
+//for (let i = 0; i < PurplePhoenixAge.length; i++) {
+ //console.log(PurplePhoenixAge[i]);
+//}
+    
+
+//Exercise 7 for...of Loop
+
+//  const purplePhoenixAge = [10, 13, 16, 15, 14, 9, 28, 22];
+//  const odds = [];
+// for (var element of purplePhoenixAge) {
+//      console.log('element :>> ', element);
+//    if (element % 2 === 1) {
+//        odds.push(element);
+//       }
+// }
+
+
+// console.log('purplePhoenixAge :>> ', purplePhoenixAge[1]);
+//  for (let i = 0; i < purplePhoenixAge.length; i++) {
+//      if (purplePhoenixAge[i]% 2 === 1) {
+//          odds.push(purplePhoenixAge[i]);
+         
+//     }
+//     console.log('odds :>> ', odds);
+//  }
+//  console.log(odds);
+
+//Exercise 7 while Loop
+
+// const PurplePhoenixAge = [10, 13, 16, 15, 14, 9, 28, 22];
+// PurplePhoenixAge.sort(function (a, b) { return a - b });
+// let i = 0;
+// let PurplePhoenixAgelength = PurplePhoenixAge.length;
+// while (i < PurplePhoenixAgelength) {
+//     if (PurplePhoenixAge[i] % 2 === 1) {
+//         console.log(PurplePhoenixAge[i]);
+//     }
+//     i++;
+// }
+
+
+// Exercise 8 Find the lowest number with a function and without Min
+// let numbers = [4, 2, 8, 9, 5];
+//  let lowestNumber = findLowestNumber(numbers);
+
+//  function findLowestNumber(numbers) {
+//      let lowestNumber = numbers[0];                    // set the first element as the initial lowest number
+     
+//    for (let i = 0; i < numbers.length; i++) {          //schleife läuft so häufig wie es parameter im array gibt hier 6x startet von 0 weil i=0
+//      if (numbers[i] < lowestNumber) {
+//        lowestNumber = numbers[i];
+//      }
+//    }
+//    return lowestNumber;
+// }
+ 
+function findMax(zahl) {
+
+var maxNumber = zahl[0]
+for (let i = 0; i < zahl.length; i++) {
+    console.log('zahl[i] :>> ', zahl[i]);
+    if (zahl[i] > maxNumber) {
+       console.log("inside if condition")
+       maxNumber = zahl[i]
+    }
+   
+    
+    
+}
+console.log('maxNumber :>> ', maxNumber);
+
 }
 
-//Exercise 7 - Looping over an Array
+let numbers = [4, 2, 8, 9, 5];
+let numbers2= [400,699,7,8,9,23003]
+ 
+findMax(numbers)
+findMax(numbers2)
+
+
+
+
+
+ console.log("The lowest in the array is: " + lowestNumber); 
+ 
+ 
+
+// E8 without a function
+
+let array = [12, 28, 33, 7, 3, 1];
+let smallest = array[0];
+
+for (let i = 0; i < array.length; i++) {
+    if (smallest > array[i]) {
+        smallest = array[i];
+    }
+}
+console.log(smallest);
